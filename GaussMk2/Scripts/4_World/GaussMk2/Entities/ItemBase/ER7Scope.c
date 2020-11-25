8c34474629c99c8f2802e0e00342a399d207a745
InclementDeath <tylerpaul2329@gmail.com>
2020-11-25 16:27:52 -0500

    Update ER7Scope.c

--- GaussMk2/Scripts/4_World/GaussMk2/Entities/ItemBase/ER7Scope.c
+++ GaussMk2/Scripts/4_World/GaussMk2/Entities/ItemBase/ER7Scope.c
@@ -49,14 +49,7 @@ class ER7ScopeLayoutHandler: ScriptedWidgetEventHandler
 		
 		// Set Range
 		range_text.SetText(distance.ToString()); 
-		
-		// Set Human Detection
-		contact_image.Show(false);
-		foreach (Object result: results) {
-			if (result.IsAlive() && result.IsMan()) {
-				contact_image.Show(true);
-			}
-		}
+
 	}
 }
 
@@ -66,18 +59,50 @@ class ER7Scope: ItemOptics
 	protected bool m_IsEnabled;	
 	protected Widget m_ScopeWidget;
 	
+	protected ref Timer m_Timer;
+	
 	override void OnWorkStart()
 	{
 		super.OnWorkStart();
-		Print("WorkStart");
 		m_ScopeWidget = GetGame().GetWorkspace().CreateWidgets("Namalsk_Weapon/GaussMk2/GUI/layouts/gauss_scope.layout");
-		Print(m_ScopeWidget);
+		
+		m_Timer = new Timer(CALL_CATEGORY_GUI);		
+		m_Timer.Run(0.1, this, "UpdateHud", null, true);
+	}
+	
+	void UpdateHud()
+	{
+		vector begin = GetGame().GetCurrentCameraPosition();
+		vector end = begin + (GetGame().GetCurrentCameraDirection() * 1000);
+		vector contact_pos, contact_dir;
+		int contact_component;
+		set<Object> results = new set<Object>();
+		
+		DayZPhysics.RaycastRV(begin, end, contact_pos, contact_dir, contact_component, results, null, GetGame().GetPlayer(), false, false);
+				
+		Print(GetHiddenSelectionIndex("contact_marker"));
+		
+		TStringArray text = GetHiddenSelectionsTextures();
+		foreach (string t: text) {
+			Print(t);
+		}
+		
+		// Set Human Detection
+		SetSimpleHiddenSelectionState(GetHiddenSelectionIndex("contact_marker"), false);
+		foreach (Object result: results) {
+			if (result.IsAlive() && result.IsMan()) {
+				SetSimpleHiddenSelectionState(GetHiddenSelectionIndex("contact_marker"), true);
+			}
+		}
 	}
 	
 	override void OnWorkStop()
 	{
 		super.OnWorkStop();
-		Print("OnWorkStop");
+		
+		m_Timer.Stop();
+		delete m_Timer;
+		
 		if (m_ScopeWidget) {
 			m_ScopeWidget.Unlink();
 		}
