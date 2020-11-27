dbad819499c6aeba8bf51f2cb8a5cf1a86ef43d2
InclementDeath <tylerpaul2329@gmail.com>
2020-11-27 15:14:25 -0500

    Update ER7Scope.c

--- GaussMk2/Scripts/4_World/GaussMk2/Entities/ItemBase/ER7Scope.c
+++ GaussMk2/Scripts/4_World/GaussMk2/Entities/ItemBase/ER7Scope.c
@@ -106,33 +106,30 @@ class C3_Gauss: FAL_Base
 	override void EEFired(int muzzleType, int mode, string ammoType)
 	{
 		Print("EEFired");
+		super.EEFired(muzzleType, mode, ammoType);
 		
 		if (GetGame().IsClient() || !GetGame().IsMultiplayer()) {
 			
-			for (int i = 0; i < 10; i++) {
+			//for (int i = 0; i < 10; i++) {
 				vector pos = GetPosition();
 				//vector ori = 180 * Vector(Math.RandomFloat01(), Math.RandomFloat01(), Math.RandomFloat01());
 				float scl = GetScale() * 0.05;
 				thread CreateFireBolt(pos, vector.Zero, scl);
-			}
+			//}
 			
 			Print("Pew electric effects!");
 		}
-		
-		super.EEFired(muzzleType, mode, ammoType);
 	}
 	
 	private void CreateFireBolt(vector position, vector orientation, float scale)
 	{
 		Print("CreateFireBolt " + position);
-		Print(orientation);
-		Print(scale);
 		Object bolt = GetGame().CreateObjectEx(GAUSS_LIGHTNING_BOLT_TYPES.GetRandomElement(), position, ECE_LOCAL);
 		if (!bolt) return;
 		bolt.SetOrientation(orientation);
 		bolt.SetScale(scale);
-		//Sleep(25);
-		//bolt.Delete();
+		Sleep(25);
+		bolt.Delete();
 		Print("Done");
 	}
 }
