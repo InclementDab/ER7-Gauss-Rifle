ba429eb1bf4dee2b756307386d6180d5cd13a26b
Jordan447 <jordan447@hotmail.co.uk>
2020-12-02 22:06:17 +0000

    removed old shit

--- GaussMk2/Scripts/4_World/WeaponAnimations.c
+++
@@ -1,8 +0,0 @@
-modded class ModItemRegisterCallbacks
-{
-    override void RegisterFireArms(DayZPlayerType pType, DayzPlayerItemBehaviorCfg pBehavior)
-    {
-        super.RegisterFireArms( pType, pBehavior )        
-        pType.AddItemInHandsProfileIK("ER7_Gauss_Base", "Namalsk_Weapon/GaussMk2/Animations/ER7_Gauss.asi", pBehavior, "dz/anims/anm/player/ik/weapons/fn_fal.anm",  "dz/anims/anm/player/reloads/fnfal/w_fnfal_states.anm");
-    }    
-};   
