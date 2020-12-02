43b53b0e6291e6fe124fd64123fdd5a10f76f0f1
Tyler Paul <tylerpaul2329@gmail.com>
2020-12-02 15:12:45 -0500

    major

--- GaussMk2/config.cpp
+++ GaussMk2/config.cpp
@@ -42,8 +42,8 @@ class Mode_Safe;
 class Mode_SemiAuto;
 class cfgWeapons
 {
-	class SKS_Base;
-	/*class ER7_Gauss_Base: SKS_Base
+	class Rifle_Base;
+	class ER7_Gauss_Base: Rifle_Base
 	{
 		scope = 0;
 		displayName = "GRW ER7 MK2";
@@ -52,8 +52,9 @@ class cfgWeapons
 		weight = 13000;
 		itemSize[] = {10,4};
 		dexterity = 1.8;
-		DisplayMagazine=0;
 		absorbency = 0.1;
+		repairableWithKits[] = {5,1};
+		repairCosts[] = {30,25};
 		discreteDistance[] = {100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000};
 		discreteDistanceInitIndex = 0;
 		distanceZoomMin = 100;
@@ -61,11 +62,21 @@ class cfgWeapons
 		PPDOFProperties[] = {0,0,0,0,0,0};
 		chamberedRound = "";
 		chamberSize = 4;
-		chamberableFrom[] = {"Ammo_ER7RFW"};
 		hiddenSelections[] = {"camo"};
 		hiddenSelectionsMaterials[] = {"Namalsk_Weapon\GaussMk2\data\gauss.rvmat"};
 		attachments[] = {"weaponOptics","weaponOpticsHunting"};
+		chamberableFrom[] = {"Ammo_ER7RFW"};
 		recoilModifier[] = {1,3,2};
+		modes[] = {"SemiAuto"};
+		class SemiAuto: Mode_SemiAuto
+		{
+			soundSetShot[] = {"ER7_Shot_SoundSet","FNFAL_Tail_SoundSet","FNFAL_InteriorTail_SoundSet"};
+			reloadTime = 0.425;
+			recoil = "recoil_AKM";
+			recoilProne = "recoil_AKM_prone";
+			dispersion = 0.00075;
+			magazineSlot = "magazine";
+		};
 		class Particles
 		{
 			class OnFire
@@ -389,12 +400,12 @@ class cfgWeapons
 				};
 			};
 		};
-	};*/
-
-	class ER7_Gauss: SKS_Base
+	};
+	class ER7_Gauss: ER7_Gauss_Base
 	{
 		scope = 2;
 		displayName = "GRW ER7 MK2";
+		hiddenSelectionsTextures[] = {"Namalsk_Weapon\GaussMk2\data\gauss_co.paa"};
 	};
 };
 
