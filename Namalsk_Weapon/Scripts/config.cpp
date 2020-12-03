3925bb1a2350a9d9f27e8bb542b4732f25fa6476
wrdg <officialwardog@gmail.com>
2020-12-02 23:03:31 -0600

    Added imageset to config.cpp

--- Namalsk_Weapon/Scripts/config.cpp
+++ Namalsk_Weapon/Scripts/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"DZ_Data","DZ_Weapons_Firearms","DZ_Weapons_Ammunition","DZ_Weapons_Magazines", "JM_CF_Scripts"};
+		requiredAddons[] = {"DZ_Data","DZ_Weapons_Firearms","DZ_Weapons_Ammunition","DZ_Weapons_Magazines","JM_CF_Scripts","Namalsk_Weapon_GaussMk2"};
 		magazines[] = {"A2GaussMk2_Magazine"};
 		ammo[] = {};
 	};
@@ -15,7 +15,7 @@ class CfgMods
 {
 	class Namalsk_Weapon
 	{
-		name = "ER7 Scripts";
+		name = "ER7 Weapon";
 		dir = "Namalsk_Weapon";
 		credits = "InclementDab & ChrisPBacon";
 		author = "InclementDab & ChrisPBacon";
@@ -26,10 +26,16 @@ class CfgMods
 		};
 		class defs
 		{
+			class imageSets
+			{
+				files[] =
+				{
+					"Namalsk_Weapon/GaussMk2/GUI/imagesets/gauss_ghost.imageset"
+				};
+			};
 			class worldScriptModule
 			{
-				value="";
-				files[] = 
+				files[] =
 				{
 					"Namalsk_Weapon/Scripts/4_World"
 				};
