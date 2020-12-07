d678d6e0ff9a133e051b911d4ee3d875cb13b74d
Tyler Paul <tylerpaul2329@gmail.com>
2020-12-07 13:40:50 -0500

    Buffed accuracy at long ranges

--- Namalsk_Weapon/GaussMk2/config.cpp
+++ Namalsk_Weapon/GaussMk2/config.cpp
@@ -48,7 +48,7 @@ class cfgWeapons
 			reloadTime = 0.475;
 			recoil = "recoil_AKM";
 			recoilProne = "recoil_AKM_prone";
-			dispersion = 0.00075;
+			dispersion = 0.0000075;
 			magazineSlot = "magazine";
 		};
         class EnergyManager
