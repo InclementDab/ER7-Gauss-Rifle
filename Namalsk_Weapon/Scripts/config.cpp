e5557235e1845d35f89c0d1530bab8ef49aa1583
Tyler Paul <tylerpaul2329@gmail.com>
2021-06-16 19:34:12 -0400

    Update config.cpp

--- Namalsk_Weapon/Scripts/config.cpp
+++ Namalsk_Weapon/Scripts/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"DZ_Scripts"};
+		requiredAddons[] = {"DZ_Scripts", "DF_Scripts"};
 		magazines[] = {};
 		ammo[] = {};
 	};
