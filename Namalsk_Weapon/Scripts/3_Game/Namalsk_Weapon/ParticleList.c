180261230251980bd5e6391c53300ed0551cf2e2
Gormirn <78990875+Gormirn@users.noreply.github.com>
2022-06-16 10:16:07 -0400

    Create ParticleList.c

---
+++ Namalsk_Weapon/Scripts/3_Game/Namalsk_Weapon/ParticleList.c
@@ -0,0 +1,5 @@
+modded class ParticleList
+{
+	static const int GAUSS_HEAT_RIGHT  = 	RegisterParticle("Namalsk_Weapon/GaussMk2/Particles/" , "Gauss_Heat_Right");
+	static const int GAUSS_HEAT_LEFT  = 	RegisterParticle("Namalsk_Weapon/GaussMk2/Particles/" , "Gauss_Heat_Left");
+}
