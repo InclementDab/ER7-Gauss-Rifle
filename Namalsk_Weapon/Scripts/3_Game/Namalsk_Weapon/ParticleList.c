4ef8336070d7204aa32563c43104aa255019558e
Gormirn <78990875+Gormirn@users.noreply.github.com>
2022-06-18 15:44:29 -0400

    Update ParticleList.c

--- Namalsk_Weapon/Scripts/3_Game/Namalsk_Weapon/ParticleList.c
+++ Namalsk_Weapon/Scripts/3_Game/Namalsk_Weapon/ParticleList.c
@@ -1,5 +1,6 @@
 modded class ParticleList
 {
+	static const int GAUSS_FIRE	= 			RegisterParticle("Namalsk_Weapon/GaussMk2/Particles/" , "Gauss_Fire");		
 	static const int GAUSS_HEAT_RIGHT  = 	RegisterParticle("Namalsk_Weapon/GaussMk2/Particles/" , "Gauss_Heat_Right");
 	static const int GAUSS_HEAT_LEFT  = 	RegisterParticle("Namalsk_Weapon/GaussMk2/Particles/" , "Gauss_Heat_Left");
 }
